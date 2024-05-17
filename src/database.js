65faa498c2c1bf20e8bcf062a61312f0b5316d13
NotAProton <git@notaproton.me>
2024-05-17 20:39:28 +0530

    Initial commit (by create-cloudflare CLI)
    Details:
      C3 = create-cloudflare@2.21.2
      project name = port0-server
      package manager = npm@10.7.0
      wrangler = wrangler@3.57.0
      git = 2.45.0

--- src/database.js
+++
@@ -1,6 +0,0 @@
-export default async function storeOTP(email, otp) {
-  const info = await env.DB.prepare('INSERT INTO users (email, otp) VALUES (?1, ?2)')
-    .bind(email, otp)
-    .run();
-  return info;
-}
