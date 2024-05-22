84b1b085d2ac5145cd27713705bedb663cf23a87
NotAProton <git@notaproton.me>
2024-05-22 05:37:09 +0530

    fix formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,9 @@
-
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
-  prettierConfig,
+	eslint.configs.recommended,
+	...tseslint.configs.strict,
+	...tseslint.configs.stylistic,
+	prettierConfig,
 );
